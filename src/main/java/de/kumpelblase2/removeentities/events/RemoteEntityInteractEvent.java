9a3d0e3c927fe34f8beed11c0e23f79539532f08
Tim H <admin@infinityblade.de>
2012-10-01 23:20:02 +0200

    Fixed interact event

--- src/main/java/de/kumpelblase2/removeentities/events/RemoteEntityInteractEvent.java
+++ src/main/java/de/kumpelblase2/removeentities/events/RemoteEntityInteractEvent.java
@@ -1,11 +1,13 @@
 package de.kumpelblase2.removeentities.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import de.kumpelblase2.removeentities.entities.RemoteEntity;
 
 public class RemoteEntityInteractEvent extends RemoteEvent
 {
 	protected final Player m_interactor;
+	private static HandlerList handlers = new HandlerList();
 	
 	public RemoteEntityInteractEvent(RemoteEntity inEntity, Player inInteractor)
 	{
@@ -16,5 +18,10 @@ public class RemoteEntityInteractEvent extends RemoteEvent
 	public Player getInteractor()
 	{
 		return this.m_interactor;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
 	}
 }
