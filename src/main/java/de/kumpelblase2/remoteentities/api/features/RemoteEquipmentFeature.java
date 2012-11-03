d5dda44fdab520cefa4d6ef0a2c9cd218107ede1
Tim H <admin@infinityblade.de>
2012-11-03 22:05:41 +0100

    Made equipment also apply on all entites

--- src/main/java/de/kumpelblase2/remoteentities/api/features/RemoteEquipmentFeature.java
+++ src/main/java/de/kumpelblase2/remoteentities/api/features/RemoteEquipmentFeature.java
@@ -1,6 +1,8 @@
 package de.kumpelblase2.remoteentities.api.features;
 
+import net.minecraft.server.EntityLiving;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.ItemStack;
 import de.kumpelblase2.remoteentities.api.RemoteEntity;
@@ -30,6 +32,15 @@ public class RemoteEquipmentFeature extends RemoteFeature implements EquipmentFe
 			HumanEntity human = (HumanEntity)this.m_entity.getBukkitEntity();
 			human.setItemInHand(this.m_equipment.getItemInHand());
 			human.getInventory().setArmorContents(this.m_equipment.getArmorParts());
+		}
+		else
+		{
+			EntityLiving entity = this.m_entity.getHandle();
+			entity.setEquipment(0, (this.m_equipment.getItemInHand() != null ? (new CraftItemStack(this.m_equipment.getItemInHand())).getHandle() : null));
+			for(int i = 4, j = 0; i >= 1; i--, j++)
+			{
+				entity.setEquipment(i, (this.m_equipment.getArmorParts()[j] != null ? (new CraftItemStack(this.m_equipment.getArmorParts()[j])).getHandle() : null));
+			}
 		}
 	}
 
