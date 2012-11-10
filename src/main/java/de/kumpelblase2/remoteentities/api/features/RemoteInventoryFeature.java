f858fa98e0c79a3082a7b6d88a37a6224f583570
Tim H <admin@infinityblade.de>
2012-11-10 23:48:46 +0100

    Made the size of the inventory adjustable

--- src/main/java/de/kumpelblase2/remoteentities/api/features/RemoteInventoryFeature.java
+++ src/main/java/de/kumpelblase2/remoteentities/api/features/RemoteInventoryFeature.java
@@ -1,7 +1,6 @@
 package de.kumpelblase2.remoteentities.api.features;
 
 import org.bukkit.craftbukkit.inventory.CraftInventoryCustom;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import de.kumpelblase2.remoteentities.api.RemoteEntity;
@@ -12,8 +11,13 @@ public class RemoteInventoryFeature extends RemoteFeature implements InventoryFe
 	
 	public RemoteInventoryFeature(RemoteEntity inEntity)
 	{
+		this(inEntity, 27);
+	}
+	
+	public RemoteInventoryFeature(RemoteEntity inEntity, int inSize)
+	{
 		super("INVENTORY", inEntity);
-		this.m_inventory = new CraftInventoryCustom((InventoryHolder)inEntity.getHandle(), InventoryType.CHEST);
+		this.m_inventory = new CraftInventoryCustom((InventoryHolder)inEntity.getHandle(), inSize);
 	}
 	
 	@Override
