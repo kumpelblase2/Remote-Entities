bb31d0cb98ea5b20a52f60d21c8ffb9fba1d3921
Tim H <admin@infinityblade.de>
2012-11-22 23:11:05 +0100

    Added default speed for bat entity

--- src/main/java/de/kumpelblase2/remoteentities/api/DefaultEntitySpeed.java
+++ src/main/java/de/kumpelblase2/remoteentities/api/DefaultEntitySpeed.java
@@ -29,4 +29,5 @@ public class DefaultEntitySpeed
 	public static final float SKELETON_SPEED = 0.25F;
 	public static final float WITCH_SPEED = 0.25F;
 	public static final float WITHER_SPEED = 0.6F;
+	public static final float BAT_SPEED = 0.25F;
 }
