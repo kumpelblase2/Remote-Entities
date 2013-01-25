83438110e8d880c73bdefa11c7cb5282a6d32d37
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-01-24 23:30:21 -0800

    Add basic properties for desires.

--- src/main/java/de/kumpelblase2/remoteentities/persistence/DesireData.java
+++ src/main/java/de/kumpelblase2/remoteentities/persistence/DesireData.java
@@ -1,5 +1,34 @@
 package de.kumpelblase2.remoteentities.persistence;
 
-public class DesireData
+import de.kumpelblase2.remoteentities.api.thinking.Desire;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DesireData implements ConfigurationSerializable
 {
+    public String desireName;
+    public int priority;
+    public int type;
+
+    public DesireData() {
+
+    }
+
+    public DesireData(Desire desire) {
+        this.desireName = desire.getClass().toString();
+        this.type = desire.getType();
+    }
+
+    @Override
+    public Map<String, Object> serialize()
+    {
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("desireName", this.desireName);
+        data.put("priority", this.priority);
+        data.put("type", this.type);
+
+        return data;
+    }
 }
