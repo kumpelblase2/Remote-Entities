49aec5e2a5394abd4838a6570de4252a02e94467
Tim H <admin@infinityblade.de>
2013-03-09 16:35:45 +0100

    Added DeisreKillTarget as first one time desire.

---
+++ src/main/java/de/kumpelblase2/remoteentities/api/thinking/goals/DesireKillTarget.java
@@ -0,0 +1,94 @@
+package de.kumpelblase2.remoteentities.api.thinking.goals;
+
+import net.minecraft.server.v1_4_R1.EntityLiving;
+import net.minecraft.server.v1_4_R1.PathEntity;
+import org.bukkit.craftbukkit.v1_4_R1.entity.CraftLivingEntity;
+import org.bukkit.entity.LivingEntity;
+import de.kumpelblase2.remoteentities.api.RemoteEntity;
+import de.kumpelblase2.remoteentities.api.thinking.DesireBase;
+import de.kumpelblase2.remoteentities.api.thinking.OneTimeDesire;
+
+public class DesireKillTarget extends DesireBase implements OneTimeDesire
+{
+	protected EntityLiving m_target;
+	protected PathEntity m_path;
+	protected int m_moveTick;
+	protected int m_attackTick = 0;
+	
+	public DesireKillTarget(RemoteEntity inEntity, LivingEntity inTarget)
+	{
+		super(inEntity);
+		this.m_target = ((CraftLivingEntity)inTarget).getHandle();
+		this.m_type = 3;
+	}
+	
+	@Deprecated
+	public DesireKillTarget(RemoteEntity inEntity)
+	{
+		super(inEntity);
+	}
+
+	@Override
+	public boolean shouldExecute()
+	{
+		if(this.getEntityHandle() == null)
+			return false;
+		
+		if(this.m_target == null)
+			return false;
+		
+		if(!this.m_entity.getBukkitEntity().getLocation().getWorld().getName().equals(this.m_target.getBukkitEntity().getWorld().getName()))
+			return false;
+		
+		this.m_path = this.getEntityHandle().getNavigation().a(this.m_target);
+		return this.m_path != null;
+	}
+	
+	@Override
+	public boolean canContinue()
+	{
+		return this.m_target.isAlive();
+	}
+	
+	@Override
+	public void startExecuting()
+	{
+		this.movePath(this.m_path, this.m_entity.getSpeed());
+		this.m_moveTick = 0;
+	}
+
+	@Override
+	public void stopExecuting()
+	{
+		this.getEntityHandle().getNavigation().g();
+	}
+	
+	@Override
+	public boolean update()
+	{
+		EntityLiving entity = this.getEntityHandle();
+		entity.getControllerLook().a(this.m_target, 30, 30);
+		if(--this.m_moveTick <= 0)
+		{
+			this.m_moveTick = 4 + entity.aB().nextInt(7);
+			this.getRemoteEntity().move((LivingEntity)entity.getBukkitEntity(), this.getRemoteEntity().getSpeed());
+		}
+		
+		this.m_attackTick = Math.max(this.m_attackTick - 1, 0);
+		double minDist = entity.width * 2 * entity.width * 2;
+		if(this.m_attackTick <= 0 && entity.e(this.m_target.locX, this.m_target.boundingBox.b, this.m_target.locZ) <= minDist)
+		{
+			this.m_attackTick = 20;
+			if(entity.bD() != null)
+				this.getEntityHandle().bH();
+			
+			this.attack((LivingEntity)this.m_target.getBukkitEntity());
+		}
+		return true;
+	}
+	
+	public void attack(LivingEntity inEntity)
+	{
+		this.getEntityHandle().m(((CraftLivingEntity)inEntity).getHandle());
+	}
+}
