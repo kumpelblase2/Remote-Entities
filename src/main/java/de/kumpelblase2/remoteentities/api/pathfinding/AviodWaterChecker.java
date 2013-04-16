db1e55ee80b9d92f768b4d607d463355f4ab17bf
Tim H <admin@infinityblade.de>
2013-04-16 12:13:53 +0200

    Added checker to avoid water.

---
+++ src/main/java/de/kumpelblase2/remoteentities/api/pathfinding/AviodWaterChecker.java
@@ -0,0 +1,23 @@
+package de.kumpelblase2.remoteentities.api.pathfinding;
+
+import org.bukkit.Material;
+
+
+public class AviodWaterChecker implements MoveChecker
+{
+	@Override
+	public void checkMove(MoveData inData)
+	{
+		if(inData.getBlock().getType() == Material.WATER || inData.getBlock().getType() == Material.STATIONARY_WATER)
+		{
+			inData.setValid(false);
+			return;
+		}
+		
+		if(inData.getAboveBlock().getType() == Material.WATER || inData.getAboveBlock().getType() == Material.STATIONARY_WATER)
+		{
+			inData.setValid(false);
+			return;
+		}
+	}
+}
