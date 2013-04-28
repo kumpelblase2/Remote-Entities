f7b3d4f96c07cac85cd1325496923c366951175f
Tim H <admin@infinityblade.de>
2013-04-28 17:30:13 +0200

    Fixed ignoreserialization annotation.

--- src/main/java/de/kumpelblase2/remoteentities/persistence/IgnoreSerialization.java
+++ src/main/java/de/kumpelblase2/remoteentities/persistence/IgnoreSerialization.java
@@ -1,5 +1,9 @@
 package de.kumpelblase2.remoteentities.persistence;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface IgnoreSerialization
 {
 }
