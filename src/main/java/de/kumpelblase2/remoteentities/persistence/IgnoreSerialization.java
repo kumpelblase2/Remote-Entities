c6bd0d00e994499b3e0d58e0cb7be620094fc822
Tim H <admin@infinityblade.de>
2013-05-06 10:31:07 +0200

    Seems like I've overlooked something.

--- src/main/java/de/kumpelblase2/remoteentities/persistence/IgnoreSerialization.java
+++ src/main/java/de/kumpelblase2/remoteentities/persistence/IgnoreSerialization.java
@@ -3,7 +3,7 @@ package de.kumpelblase2.remoteentities.persistence;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target(ElementType.TYPE)
 public @interface IgnoreSerialization
 {
 }
