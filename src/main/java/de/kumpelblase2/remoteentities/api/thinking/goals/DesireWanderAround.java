3b053932ca6f14c27e6e60421e9b4d9fac5a90cb
Tim H <admin@infinityblade.de>
2013-11-17 01:01:38 +0100

    Add custom speed for wander around.

--- src/main/java/de/kumpelblase2/remoteentities/api/thinking/goals/DesireWanderAround.java
+++ src/main/java/de/kumpelblase2/remoteentities/api/thinking/goals/DesireWanderAround.java
@@ -7,6 +7,8 @@ import de.kumpelblase2.remoteentities.api.RemoteEntity;
 import de.kumpelblase2.remoteentities.api.thinking.DesireBase;
 import de.kumpelblase2.remoteentities.api.thinking.DesireType;
 import de.kumpelblase2.remoteentities.nms.RandomPositionGenerator;
+import de.kumpelblase2.remoteentities.persistence.SerializeAs;
+import de.kumpelblase2.remoteentities.utilities.NMSUtil;
 
 /**
  * Using this desire the entity will move around randomly.
@@ -16,6 +18,8 @@ public class DesireWanderAround extends DesireBase
 	protected double m_xPos;
 	protected double m_yPos;
 	protected double m_zPos;
+	@SerializeAs(pos = 0)
+	protected float m_speed = -1;
 
 	@Deprecated
 	public DesireWanderAround(RemoteEntity inEntity)
@@ -28,6 +32,12 @@ public class DesireWanderAround extends DesireBase
 	{
 		super();
 		this.m_type = DesireType.PRIMAL_INSTINCT;
+	}
+
+	public DesireWanderAround(float inSpeed)
+	{
+		this();
+		this.m_speed = inSpeed;
 	}
 
 	@Override
@@ -67,6 +77,6 @@ public class DesireWanderAround extends DesireBase
 	@Override
 	public void startExecuting()
 	{
-		this.getRemoteEntity().move(new Location(this.getRemoteEntity().getBukkitEntity().getWorld(), this.m_xPos, this.m_yPos, this.m_zPos));
+		this.getRemoteEntity().move(new Location(this.getRemoteEntity().getBukkitEntity().getWorld(), this.m_xPos, this.m_yPos, this.m_zPos), (this.m_speed == -1 ? this.getRemoteEntity().getSpeed() : this.m_speed));
 	}
 }
