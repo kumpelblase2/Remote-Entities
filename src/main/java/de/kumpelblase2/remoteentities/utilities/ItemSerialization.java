aab3d2a9b3eda7aba73c07b98323254b062accb9
Tim H <admin@infinityblade.de>
2014-01-08 14:10:01 +0100

    Changed item serialization.

--- src/main/java/de/kumpelblase2/remoteentities/utilities/ItemSerialization.java
+++ src/main/java/de/kumpelblase2/remoteentities/utilities/ItemSerialization.java
@@ -1,19 +1,26 @@
 package de.kumpelblase2.remoteentities.utilities;
 
 import java.io.*;
+import java.lang.reflect.Method;
 import java.math.BigInteger;
 import net.minecraft.server.v1_7_R1.*;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_7_R1.inventory.CraftInventoryCustom;
 import org.bukkit.craftbukkit.v1_7_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 /**
  * Made by Comprehenix.
- * Code can be found at: https://gist.github.com/aadnk/4102407
- * Slightly modified to work with newest bukkit version.
+ * Code can be found at: https://gist.github.com/aadnk/8138345
  */
 public class ItemSerialization {
+
+	private static Method WRITE_NBT;
+	private static Method READ_NBT;
 
 	/**
 	 * Serializes an inventory to a encoded string.
@@ -23,6 +30,7 @@ public class ItemSerialization {
 	 */
 	public static String toString(Inventory inventory) {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		DataOutputStream dataOutput = new DataOutputStream(outputStream);
 		NBTTagList itemList = new NBTTagList();
 
 		// Save every element in the list
@@ -33,17 +41,14 @@ public class ItemSerialization {
 			// Convert the item stack to a NBT compound
 			if (craft != null)
 				CraftItemStack.asNMSCopy(craft).save(outputObject);
-
 			itemList.add(outputObject);
 		}
 
 		// Now save the list
-		NBTTagCompound whole = new NBTTagCompound();
-		whole.set("Inventory", itemList);
-		NBTCompressedStreamTools.a(whole, outputStream);
+		writeNbt(itemList, dataOutput);
 
 		// Serialize that array
-		return new BigInteger(1, outputStream.toByteArray()).toString(32);
+		return Base64Coder.encodeLines(outputStream.toByteArray());
 	}
 
 	/**
@@ -52,22 +57,56 @@ public class ItemSerialization {
 	 * @param data  String to deserialize
 	 * @return      Deserialized inventory
 	 */
-	public static Inventory fromString(String data) {
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(new BigInteger(data, 32).toByteArray());
-		NBTTagList itemList = NBTCompressedStreamTools.a(inputStream).getList("Inventory", 10);
+	public static Inventory fromString(String data) throws IOException {
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+		NBTTagList itemList = (NBTTagList) readNbt(new DataInputStream(inputStream), 0);
 		Inventory inventory = new CraftInventoryCustom(null, itemList.size());
 
 		for (int i = 0; i < itemList.size(); i++) {
-			NBTTagCompound inputObject = itemList.get(i);
+			NBTTagCompound inputObject = (NBTTagCompound) itemList.get(i);
 
-			// IsEmpty
 			if (!inputObject.isEmpty()) {
-				inventory.setItem(i, CraftItemStack.asBukkitCopy(net.minecraft.server.v1_7_R1.ItemStack.createStack(inputObject)));
+				inventory.setItem(i, CraftItemStack.asCraftMirror(
+						net.minecraft.server.v1_7_R1.ItemStack.createStack(inputObject)));
 			}
 		}
 
 		// Serialize that array
 		return inventory;
+	}
+
+	private static void writeNbt(NBTBase base, DataOutput output) {
+		if (WRITE_NBT == null) {
+			try {
+				WRITE_NBT = NBTCompressedStreamTools.class.getDeclaredMethod("a", NBTBase.class, DataOutput.class);
+				WRITE_NBT.setAccessible(true);
+			} catch (Exception e) {
+				throw new IllegalStateException("Unable to find private write method.", e);
+			}
+		}
+
+		try {
+			WRITE_NBT.invoke(null, base, output);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unable to write " + base + " to " + output, e);
+		}
+	}
+
+	private static NBTBase readNbt(DataInput input, int level) {
+		if (READ_NBT == null) {
+			try {
+				READ_NBT = NBTCompressedStreamTools.class.getDeclaredMethod("a", DataInput.class, int.class);
+				READ_NBT.setAccessible(true);
+			} catch (Exception e) {
+				throw new IllegalStateException("Unable to find private read method.", e);
+			}
+		}
+
+		try {
+			return (NBTBase) READ_NBT.invoke(null, input, level);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unable to read from " + input, e);
+		}
 	}
 
 	private static CraftItemStack getCraftVersion(ItemStack stack) {
